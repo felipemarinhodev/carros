d0704605965c212078bc3a011e536c5add428f29
Felipe Marinho <felipmarinho@gmail.com>
2019-11-17 16:53:21 -0300

    Criando a pagina de login

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body()
+    );
+  }
+
+  _body() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          _text("Login", "Digite o login"),
+          SizedBox(height: 10,),
+          _text("Senha", "Digite a senha", password: true),
+          SizedBox(height: 20,),
+          _button("Login"),
+        ],
+      ),
+    );
+  }
+
+  _text(String label, String hint, { bool password = false }) {
+    return TextFormField(
+      obscureText: password,
+      style: TextStyle(
+        fontSize: 25,
+        color: Colors.blue
+      ),
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          fontSize: 25, 
+          color: Colors.grey
+        ),
+        hintText: hint,
+        hintStyle: TextStyle (
+          fontSize: 16
+        )
+      ),
+    );
+  }
+
+  _button(String text) {
+    return Container(
+      height: 46,
+      child: RaisedButton(
+        color: Colors.blue,
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 22),
+          ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
