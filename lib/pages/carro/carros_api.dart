dd2327b2d81aa05bc9b312656bbdb93acf1581ec
Felipe Marinho <felipmarinho@gmail.com>
2019-11-30 19:24:30 -0300

    Entendendo o uso do Future e FutureBuilder

--- lib/pages/carro/carros_api.dart
+++ lib/pages/carro/carros_api.dart
@@ -1,25 +1,27 @@
 import 'package:carros/pages/carro/carro.dart';
 
 class CarrosApi {
-  static List<Carro> getCarros() {
+  static Future<List<Carro>> getCarros() async {
     final carros = List<Carro>();
 
+    await Future.delayed(Duration(seconds: 3));
+
     carros.add(Carro(
-        nome: "Porsche Panamera",
-        urlFoto:
-            "http://www.livroandroid.com.br/livro/carros/esportivos/Porsche_Panamera.png"));
+      nome: "Porsche Panamera",
+      urlFoto:
+        "http://www.livroandroid.com.br/livro/carros/esportivos/Porsche_Panamera.png"));
     carros.add(Carro(
-        nome: "Chevrolet Corvette Z06",
-        urlFoto:
-            "http://www.livroandroid.com.br/livro/carros/esportivos/Chevrolet_Corvette_Z06.png"));
+      nome: "Chevrolet Corvette Z06",
+      urlFoto:
+        "http://www.livroandroid.com.br/livro/carros/esportivos/Chevrolet_Corvette_Z06.png"));
     carros.add(Carro(
-        nome: "BMW M5",
-        urlFoto:
-            "http://www.livroandroid.com.br/livro/carros/esportivos/BMW.png"));
+      nome: "BMW M5",
+      urlFoto:
+        "http://www.livroandroid.com.br/livro/carros/esportivos/BMW.png"));
     carros.add(Carro(
-        nome: "Renault Megane RS Trophy",
-        urlFoto:
-            "http://www.livroandroid.com.br/livro/carros/esportivos/Renault_Megane_Trophy.png"));
+      nome: "Renault Megane RS Trophy",
+      urlFoto:
+        "http://www.livroandroid.com.br/livro/carros/esportivos/Renault_Megane_Trophy.png"));
 
     return carros;
   }
