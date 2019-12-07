956e4cc628fd7c669c0bd5364edbf61d88630204
Felipe Marinho <felipmarinho@gmail.com>
2019-12-06 21:00:42 -0300

    Organizando os carros por tipo utilzando abas

--- lib/pages/carro/carros_api.dart
+++ lib/pages/carro/carros_api.dart
@@ -2,11 +2,17 @@ import 'package:carros/pages/carro/carro.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
+class TipoCarro {
+  static final String classicos = "classicos";
+  static final String esportivos = "esportivos";
+  static final String luxo = "luxo";
+}
 class CarrosApi {
-  static Future<List<Carro>> getCarros() async {
-    var url = 'http://carros-springboot.herokuapp.com/api/v1/carros';
+  static Future<List<Carro>> getCarros(String tipo) async {
+    var url = 'http://carros-springboot.herokuapp.com/api/v1/carros/tipo/$tipo';
 
     print('GET > $url');
+
 
     var response = await http.get(url);
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,103 +1,31 @@
 import 'package:carros/drawer_list.dart';
-import 'package:carros/pages/carro/carro.dart';
 import 'package:carros/pages/carro/carros_api.dart';
+import 'package:carros/pages/carro/carros_listview.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Carros"),
-      ),
-      body: _body(),
-      drawer: DrawerList(),
-    );
-  }
-
-  _body() {
-    Future<List<Carro>> future = CarrosApi.getCarros();
-
-    return FutureBuilder(
-      future: future,
-      builder: (context, snapshot) {
-
-        if (snapshot.hasError) {
-          return Center(
-            child: Text(
-              'Não foi possível buscar os carros',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 25
-              ),
+    return DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Carros"),
+              bottom: TabBar(tabs: [
+                Tab(text: 'Clássicos',),
+                Tab(text: 'Esportivos',),
+                Tab(text: 'Luxo',),
+              ]),
             ),
-          );
-        }
-
-        if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        List<Carro> carros = snapshot.data;
-        return _listView(carros);
-      },
+          body: TabBarView(children: <Widget>[
+            CarrosListView(TipoCarro.classicos),
+            CarrosListView(TipoCarro.esportivos),
+            CarrosListView(TipoCarro.luxo),
+          ],),
+          drawer: DrawerList(),
+          ),
     );
   }
 
-  Container _listView(List<Carro> carros) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView.builder(
-          itemCount: carros != null ? carros.length : 0,
-          itemBuilder: (context, index) {
-            Carro c = carros[index];
-
-            return Card(
-              color: Colors.grey[100],
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Center(
-                      child: Image.network(
-                        c.urlFoto ??
-                       "http://www.livroandroid.com.br/livro/carros/classicos/Ford_Mustang.png",
-                        width: 250,
-                      ),
-                    ),
-                    Text(
-                      c.nome,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 25),
-                    ),
-                    Text(
-                      'Descrição...',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    ButtonTheme.bar(
-                      // make buttons use the appropriate styles for cards
-                      child: ButtonBar(
-                        children: <Widget>[
-                          FlatButton(
-                            child: const Text('DETALHES'),
-                            onPressed: () {/* ... */},
-                          ),
-                          FlatButton(
-                            child: const Text('SHARE'),
-                            onPressed: () {/* ... */},
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }),
-    );
-  }
+  
 }
